35a62d6fbf73609fba8e9cb666942e3d69a08e69
Luan Motta <luanmtrs@gmail.com>
2021-06-15 15:08:26 -0300

    [feat] Access Control working with ADULM strategy

--- src/interfaces/Endpoint.ts
+++ src/interfaces/Endpoint.ts
@@ -1,7 +1,7 @@
 export interface Endpoint {
   id: string;
   name: string;
-  action: "Search" | "Insert" | "Update" | "Delete";
+  action: "Search" | "GetById" | "Insert" | "Update" | "Delete";
   application: string;
   container: string;
   description: string;
