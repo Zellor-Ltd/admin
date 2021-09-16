52339cbce1403a7fe2a060bf7833afa6dc6d8707
unknown <rochelle.beatriz@gmail.com>
2021-09-16 16:16:10 -0300

    content separated into tabs

--- src/pages/brands/BrandDetail.tsx
+++ src/pages/brands/BrandDetail.tsx
@@ -9,6 +9,7 @@ import {
   Radio,
   RadioChangeEvent,
   Row,
+  Tabs,
   Select,
   Switch,
 } from "antd";
@@ -60,114 +61,164 @@ const BrandDetail: React.FC<RouteComponentProps> = (props) => {
         initialValues={initial}
         onFinish={onFinish}
       >
-        <Row gutter={8}>
-          <Col lg={12} xs={24}>
-            <Col lg={16} xs={24}>
-              <Form.Item label="Brand Name" name="brandName">
-                <Input />
-              </Form.Item>
-            </Col>
-            <Col lg={16} xs={24}>
-              <Form.Item
-                name="checkoutType"
-                label="Checkout Type"
-                rules={[{ required: true }]}
-              >
-                <Radio.Group
-                  buttonStyle="solid"
-                  onChange={handleCheckoutTypeChange}
-                >
-                  <Radio.Button value="internal">Internal</Radio.Button>
-                  <Radio.Button value="external">External</Radio.Button>
-                </Radio.Group>
-              </Form.Item>
-            </Col>
-            <Col lg={16} xs={24}>
-              <Form.Item
-                name="checkout"
-                label="Checkout"
-                rules={[{ required: true }]}
-              >
-                <Select placeholder="Select a checkout type">
-                  {checkoutType.map((curr: any) => (
-                    <Select.Option key={curr.value} value={curr.value}>
-                      {curr.name}
-                    </Select.Option>
-                  ))}
-                </Select>
-              </Form.Item>
-            </Col>
-            <Col lg={16} xs={24}>
-              <Form.Item
-                name="confirmationUrl"
-                label="External Payment Confirmation URL"
-                rules={[{ required: true }]}
-              >
-                <Input />
-              </Form.Item>
-            </Col>
-            <Col lg={16} xs={24}>
-              <Form.Item
-                name="cancelationUrl"
-                label="External Payment Cancelation URL"
-                rules={[{ required: true }]}
-              >
-                <Input />
-              </Form.Item>
-            </Col>
-            <Col lg={16} xs={24}>
-              <Form.Item
-                shouldUpdate={(prevValues, curValues) =>
-                  prevValues.checkout !== curValues.checkout
-                }
-              >
-                {({ getFieldValue }) => (
-                  <Form.Item
-                    name="requireMobilePurchaseStatus"
-                    label="Log Completed Purchases?"
-                    valuePropName="checked"
-                  >
-                    <Switch disabled={getFieldValue("checkout") === "Disco"} />
-                  </Form.Item>
-                )}
-              </Form.Item>
-            </Col>
-            <Col lg={8} xs={8}>
-              <Form.Item
-                name="discoPercentage"
-                label="Disco Percentage %"
-                rules={[{ required: true }]}
-              >
-                <InputNumber />
-              </Form.Item>
-            </Col>
-            <Col lg={16} xs={24}>
-              <Form.Item
-                name="shopName"
-                label="Shop Name (without https:// or spaces)"
-                rules={[{ required: true }]}
-              >
-                <Input placeholder="casey-temp.myshopify.com" />
-              </Form.Item>
-            </Col>
-            <Col lg={24} xs={24}>
-              <Form.Item label="Pre-Checkout Message (With Discount)">
-                <RichTextEditor
-                  formField="overlayHtmlWithDiscount"
-                  form={form}
-                />
-              </Form.Item>
-            </Col>
-            <Col lg={24} xs={24}>
-              <Form.Item label="Pre-Checkout Message (WithOUT Discount)">
-                <RichTextEditor
-                  formField="overlayHtmlWithoutDiscount"
-                  form={form}
-                />
-              </Form.Item>
-            </Col>
-          </Col>
 
+        <Tabs defaultActiveKey="Details">
+          <Tabs.TabPane tab="Details" key="Details">
+            <Row gutter={8}>
+              <Col lg={3} xs={3}>
+                <Form.Item
+                  name="automated"
+                  label="Automated"
+                  valuePropName="checked"
+                >
+                  <Switch />
+                </Form.Item>
+              </Col>
+              <Col lg={3} xs={3}>
+                <Form.Item
+                  name="paused"
+                  label="Paused"
+                  valuePropName="checked"
+                >
+                  <Switch />
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={8}>
+              <Col lg={12} xs={24}>
+                <Col lg={16} xs={24}>
+                  <Form.Item label="Brand Name" name="brandName">
+                    <Input />
+                  </Form.Item>
+                </Col>
+              </Col>
+              <Col lg={24} xs={24}>
+                <Form.Item
+                  label="Brand Color"
+                  name="brandTxtColor"
+                  rules={[{ required: true }]}
+                  valuePropName="color"
+                >
+                  <ColorPicker />
+                </Form.Item>
+              </Col>                
+            </Row>
+          </Tabs.TabPane>
+
+          <Tabs.TabPane tab="Checkout" key="Checkout">
+          <Row gutter={8}>
+            <Col lg={12} xs={24}>
+              <Col lg={16} xs={24}>
+                <Form.Item label="Brand Name" name="brandName">
+                  <Input />
+                </Form.Item>
+              </Col>
+              <Col lg={16} xs={24}>
+                <Form.Item
+                  name="checkoutType"
+                  label="Checkout Type"
+                  rules={[{ required: true }]}
+                >
+                  <Radio.Group
+                    buttonStyle="solid"
+                    onChange={handleCheckoutTypeChange}
+                  >
+                    <Radio.Button value="internal">Internal</Radio.Button>
+                    <Radio.Button value="external">External</Radio.Button>
+                  </Radio.Group>
+                </Form.Item>
+              </Col>
+              <Col lg={16} xs={24}>
+                <Form.Item
+                  name="checkout"
+                  label="Checkout"
+                  rules={[{ required: true }]}
+                >
+                  <Select placeholder="Select a checkout type">
+                    {checkoutType.map((curr: any) => (
+                      <Select.Option key={curr.value} value={curr.value}>
+                        {curr.name}
+                      </Select.Option>
+                    ))}
+                  </Select>
+                </Form.Item>
+              </Col>
+              <Col lg={16} xs={24}>
+                <Form.Item
+                  name="confirmationUrl"
+                  label="External Payment Confirmation URL"
+                  rules={[{ required: true }]}
+                >
+                  <Input />
+                </Form.Item>
+              </Col>
+              <Col lg={16} xs={24}>
+                <Form.Item
+                  name="cancelationUrl"
+                  label="External Payment Cancelation URL"
+                  rules={[{ required: true }]}
+                >
+                  <Input />
+                </Form.Item>
+              </Col>
+              <Col lg={16} xs={24}>
+                <Form.Item
+                  shouldUpdate={(prevValues, curValues) =>
+                    prevValues.checkout !== curValues.checkout
+                  }
+                >
+                  {({ getFieldValue }) => (
+                    <Form.Item
+                      name="requireMobilePurchaseStatus"
+                      label="Log Completed Purchases?"
+                      valuePropName="checked"
+                    >
+                      <Switch disabled={getFieldValue("checkout") === "Disco"} />
+                    </Form.Item>
+                  )}
+                </Form.Item>
+              </Col>
+              <Col lg={8} xs={8}>
+                <Form.Item
+                  name="discoPercentage"
+                  label="Disco Percentage %"
+                  rules={[{ required: true }]}
+                >
+                  <InputNumber />
+                </Form.Item>
+              </Col>
+              <Col lg={16} xs={24}>
+                <Form.Item
+                  name="shopName"
+                  label="Shop Name (without https:// or spaces)"
+                  rules={[{ required: true }]}
+                >
+                  <Input placeholder="casey-temp.myshopify.com" />
+                </Form.Item>
+              </Col>
+            </Col>
+            <Col lg={12} xs={24}>
+              <Col lg={24} xs={24}>
+                <Form.Item label="Pre-Checkout Message (With Discount)">
+                  <RichTextEditor
+                    formField="overlayHtmlWithDiscount"
+                    form={form}
+                  />
+                </Form.Item>
+              </Col>
+              <Col lg={24} xs={24}>
+                <Form.Item label="Pre-Checkout Message (WithOUT Discount)">
+                  <RichTextEditor
+                    formField="overlayHtmlWithoutDiscount"
+                    form={form}
+                  />
+                </Form.Item>
+              </Col>
+            </Col>
+          </Row>
+          </Tabs.TabPane>
+          
+          <Tabs.TabPane tab="Images" key="Images">   
           <Col lg={12} xs={24}>
             <Row>
               <Col lg={6} xs={24}>
@@ -242,41 +293,10 @@ const BrandDetail: React.FC<RouteComponentProps> = (props) => {
                   />
                 </Form.Item>
               </Col>
-              <Col lg={24} xs={24}>
-                <Form.Item
-                  label="Brand Color"
-                  name="brandTxtColor"
-                  rules={[{ required: true }]}
-                  valuePropName="color"
-                >
-                  <ColorPicker />
-                </Form.Item>
-              </Col>
-              <Col lg={24} xs={24}>
-                <Row gutter={8}>
-                  <Col lg={12} xs={12}>
-                    <Form.Item
-                      name="automated"
-                      label="Automated"
-                      valuePropName="checked"
-                    >
-                      <Switch />
-                    </Form.Item>
-                  </Col>
-                  <Col lg={12} xs={12}>
-                    <Form.Item
-                      name="paused"
-                      label="Paused"
-                      valuePropName="checked"
-                    >
-                      <Switch />
-                    </Form.Item>
-                  </Col>
-                </Row>
-              </Col>
             </Row>
-          </Col>
-        </Row>
+            </Col>
+          </Tabs.TabPane>
+        </Tabs>
         <Row gutter={8}>
           <Col>
             <Button type="default" onClick={() => history.goBack()}>
