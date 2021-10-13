56fb4d2ff897b766b7fb92d1a62677dd7f5d13c1
beatrizrochelle <rochelle.beatriz@gmail.com>
2021-10-13 19:45:32 -0300

    productsperday graph set up, need EP

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -23,14 +23,21 @@ const Dashboard: React.FC<DashboardProps> = () => {
   const [fansPerDay, setFansPerDay] = useState<any[]>([]);
   const [, setLoading] = useState<boolean>(true);
   const { doFetch } = useRequest({ setLoading });
+  const [productsPerDay, setProductsPerDay] = useState<any[]>([]);
 
-  const getResources = async () => {
+  const getFans = async () => {
     const { results } = await doFetch(fetchActiveRegFansPerDay);
     setFansPerDay(results);
   };
 
+  const getProducts = async () => {
+    const { results } = await doFetch(fetchProductsPerDay);
+    setProductsPerDay(results);
+  };
+
   useEffect(() => {
-    getResources();
+    getFans();
+    getProducts();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -112,6 +119,36 @@ const Dashboard: React.FC<DashboardProps> = () => {
         </Col>
         <Col style={{ width: "25%" }}>
           <Pie></Pie>
+        </Col>
+      </Row>
+      <Row style={{ height: "50%" }}>
+        <Col style={{ width: "75%" }}>
+          <ResponsiveContainer>
+            <LineChart
+              width={500}
+              height={300}
+              data={productsPerDay}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="date" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey="products"
+                name="Products added per day"
+                stroke="#8884d8"
+                activeDot={{ r: 8 }}
+              />
+            </LineChart>
+          </ResponsiveContainer>
         </Col>
       </Row>
       <Row style={{ height: "50%" }}>
