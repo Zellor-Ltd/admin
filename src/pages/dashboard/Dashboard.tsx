f4fa095fb399cc1017ca66f7e833337f847afb76
beatrizrochelle <rochelle.beatriz@gmail.com>
2021-10-14 11:31:52 -0300

    products per day graph added

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -13,7 +13,10 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
-import { fetchActiveRegFansPerDay } from "services/DiscoClubService";
+import {
+  fetchActiveRegFansPerDay,
+  fetchProductsPerDay,
+} from "services/DiscoClubService";
 import Pie from "./Pie";
 import Radar from "./Radar";
 
@@ -40,51 +43,6 @@ const Dashboard: React.FC<DashboardProps> = () => {
     getProducts();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  const data = [
-    {
-      name: "Page A",
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: "Page B",
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: "Page C",
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: "Page D",
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: "Page E",
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: "Page F",
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: "Page G",
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
 
   return (
     <>
@@ -122,9 +80,9 @@ const Dashboard: React.FC<DashboardProps> = () => {
         </Col>
       </Row>
       <Row style={{ height: "50%" }}>
-        <Col style={{ width: "75%" }}>
-          <ResponsiveContainer>
-            <LineChart
+        <Col style={{ width: "60%" }}>
+          <ResponsiveContainer width="100%" height="100%">
+            <BarChart
               width={500}
               height={300}
               data={productsPerDay}
@@ -140,38 +98,7 @@ const Dashboard: React.FC<DashboardProps> = () => {
               <YAxis />
               <Tooltip />
               <Legend />
-              <Line
-                type="monotone"
-                dataKey="products"
-                name="Products added per day"
-                stroke="#8884d8"
-                activeDot={{ r: 8 }}
-              />
-            </LineChart>
-          </ResponsiveContainer>
-        </Col>
-      </Row>
-      <Row style={{ height: "50%" }}>
-        <Col style={{ width: "60%" }}>
-          <ResponsiveContainer width="100%" height="100%">
-            <BarChart
-              width={500}
-              height={300}
-              data={data}
-              margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-              }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Bar dataKey="pv" fill="#8884d8" />
-              <Bar dataKey="uv" fill="#82ca9d" />
+              <Bar dataKey="products" fill="#8884d8" />
             </BarChart>
           </ResponsiveContainer>
         </Col>
