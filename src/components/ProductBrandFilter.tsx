d775751ad495726f2e6413a6f6b0285fdcdb4baf
sjolandr <rochelle.beatriz@gmail.com>
2021-11-25 20:39:59 -0300

    master brand filter working, 3 new filters in place

---
+++ src/components/ProductBrandFilter.tsx
@@ -0,0 +1,66 @@
+import { Typography } from "antd";
+import Select from "antd/lib/select";
+import React, { useEffect, useState } from "react";
+import { ProductBrand } from "../interfaces/ProductBrand";
+import { fetchProductBrands } from "../services/DiscoClubService";
+
+type ProductBrandFilterProps = Omit<
+  React.SelectHTMLAttributes<HTMLSelectElement>,
+  "onChange"
+> & {
+  onChange: (selectedBrand: ProductBrand) => {};
+  allowClear?: boolean;
+  initialProductBrandName?: string;
+  label?: string;
+};
+
+export const ProductBrandFilter: React.FC<ProductBrandFilterProps> = ({
+  onChange,
+  placeholder = "Select a Brand",
+  style,
+  allowClear = true,
+  initialProductBrandName,
+  label = "Product Brand",
+}) => {
+  const [productBrands, setProductBrands] = useState<ProductBrand[]>([]);
+  const [selectedProductBrandName, setSelectedProductBrandName] =
+    useState<string>(initialProductBrandName as string);
+
+  const _onChange = (value: string) => {
+    setSelectedProductBrandName(value);
+    onChange(productBrands.find((brand) => brand.id === value) as ProductBrand);
+  };
+
+  useEffect(() => {
+    const getProductBrands = async () => {
+      try {
+        const { results }: any = await fetchProductBrands();
+        setProductBrands(results);
+      } catch (e) {}
+    };
+    getProductBrands();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div style={{ marginBottom: "16px" }}>
+      <Typography.Title level={5} title={label}>
+        {label}
+      </Typography.Title>
+      <Select
+        value={selectedProductBrandName}
+        onChange={_onChange}
+        showSearch
+        allowClear={allowClear}
+        style={style}
+        placeholder={placeholder}
+      >
+        {productBrands.map(({ brandName, id }) => (
+          <Select.Option key={brandName} value={id}>
+            {brandName}
+          </Select.Option>
+        ))}
+      </Select>
+    </div>
+  );
+};
