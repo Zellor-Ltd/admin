b3c512871618fcc61c3d0cc7de270808e5866556
Ygor Azevedo <ygordanniel@gmail.com>
2021-12-17 07:57:17 -0300

    feat: Enhance TagForm Tag's select search ✨
    Created `DebounceSelect` component to do async search. Now Tags on `TagForm` are searched asynchronously.
    
    DIS-66

---
+++ src/components/DebounceSelect.tsx
@@ -0,0 +1,91 @@
+import React, { useMemo, useRef, useState } from 'react';
+
+import { Select, Spin } from 'antd';
+import debounce from 'lodash/debounce';
+import { useMount } from 'react-use';
+
+interface SelectOption {
+  label: string;
+  value: string;
+}
+
+interface DebounceSelectProps {
+  fetchOptions: (search: string) => Promise<any[]>;
+  onChange: (value: string, entity?: any) => void;
+  optionsMapping: SelectOption;
+  disabled?: boolean;
+  value?: string;
+  debounceTimeout?: number;
+}
+
+const DebounceSelect: React.FC<DebounceSelectProps> = ({
+  fetchOptions,
+  onChange,
+  optionsMapping,
+  disabled,
+  value,
+  debounceTimeout = 800,
+}) => {
+  const [fetching, setFetching] = useState(false);
+  const [options, setOptions] = useState<SelectOption[]>([]);
+  const fetchedEntities = useRef<any[]>([]);
+  const fetchRef = useRef(0);
+
+  useMount(() => {
+    if (value) {
+      debounceFetcher(value);
+    }
+  });
+
+  const debounceFetcher = useMemo(() => {
+    const loadOptions = (value: string) => {
+      fetchRef.current += 1;
+      const fetchId = fetchRef.current;
+      setOptions([]);
+      setFetching(true);
+
+      fetchOptions(value).then(entities => {
+        if (fetchId !== fetchRef.current) {
+          // for fetch callback order
+          return;
+        }
+
+        fetchedEntities.current = entities;
+        setOptions(
+          entities.map(option => {
+            return {
+              label: option[optionsMapping.label],
+              value: option[optionsMapping.value],
+            };
+          })
+        );
+        setFetching(false);
+      });
+    };
+
+    return debounce(loadOptions, debounceTimeout);
+  }, [fetchOptions, debounceTimeout]);
+
+  const _onChange = (option: { value: string; label: string; key: string }) => {
+    const selectedEntity = fetchedEntities.current.find(
+      entity => entity[optionsMapping.value] === option.value
+    );
+    onChange(option.value, selectedEntity);
+  };
+
+  return (
+    <Select
+      placeholder="Type to search a Tag"
+      labelInValue
+      showSearch={true}
+      filterOption={false}
+      onChange={_onChange}
+      onSearch={debounceFetcher}
+      notFoundContent={fetching ? <Spin size="small" /> : null}
+      options={options}
+      disabled={disabled}
+    />
+  );
+};
+
+export default DebounceSelect;
--- src/pages/video-feed/VideoFeed.tsx
+++ src/pages/video-feed/VideoFeed.tsx
@@ -35,7 +35,6 @@ import {
   fetchBrands,
   fetchCategories,
   fetchCreators,
-  fetchTags,
   fetchVideoFeed,
   rebuildAllFeedd,
   saveVideoFeed,
@@ -99,7 +98,6 @@ const VideoFeed: React.FC<RouteComponentProps> = ({ history, location }) => {
   const [selectedBrandIndex, setSelectedBrandIndex] = useState<number>(-1);
   const [showBrandForm, setShowBrandForm] = useState<boolean>(false);
 
-  const [tags, setTags] = useState<Tag[]>([]);
   const [selectedTag, setSelectedTag] = useState<Tag | undefined>();
   const [selectedTagIndex, setSelectedTagIndex] = useState<number>(-1);
   const [showTagForm, setShowTagForm] = useState<boolean>(false);
@@ -167,17 +165,8 @@ const VideoFeed: React.FC<RouteComponentProps> = ({ history, location }) => {
       const response: any = await fetchBrands();
       setBrands(response.results);
     }
-    async function getTags() {
-      const response: any = await fetchTags({});
-      setTags(response.results);
-    }
     setLoading(true);
-    await Promise.all([
-      getInfluencers(),
-      getCategories(),
-      getBrands(),
-      getTags(),
-    ]);
+    await Promise.all([getInfluencers(), getCategories(), getBrands()]);
     setLoading(false);
   };
 
@@ -210,14 +199,15 @@ const VideoFeed: React.FC<RouteComponentProps> = ({ history, location }) => {
           selectedTagIndex > -1 ? `${selectedTagIndex + 1} Update` : 'Creation'
         }`
       );
-    else{
+    else {
       setPageTitle(
         currentItem
           ? currentItem?.title.length > 50
             ? `${currentItem.title.slice(0, 50)} (...) Update`
             : `${currentItem.title} Update`
           : 'Update'
-      );}
+      );
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedSegment, showBrandForm, showTagForm, currentItem]);
 
@@ -237,7 +227,7 @@ const VideoFeed: React.FC<RouteComponentProps> = ({ history, location }) => {
       delete segment.package;
       return segment;
     });
-    // item.validity = moment(item.validity).format("DD/MM/YYYY");
+
     await doRequest(() => saveVideoFeed(item));
 
     resetForm();
@@ -905,7 +895,6 @@ const VideoFeed: React.FC<RouteComponentProps> = ({ history, location }) => {
             setShowTagForm={setShowTagForm}
             tag={selectedTag}
             brands={brands}
-            tags={tags}
           />
         )}
         <Form
