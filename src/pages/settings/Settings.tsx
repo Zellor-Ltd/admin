f8eabe535449c055042272b969ffbf0ec7d16c7f
Beatriz Rochelle <rochelle.beatriz@gmail.com>
2022-05-30 13:42:06 -0300

    chore: renamed tabs in settings

--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -61,13 +61,13 @@ const Settings: React.FC = () => {
           <TabPane tab="Language" key="language">
             <ItemList name="language" />
           </TabPane>
-          <TabPane tab="Orders Status" key="orderStatus">
+          <TabPane tab="Order Status" key="orderStatus">
             <ItemList name="orderStatus" />
           </TabPane>
-          <TabPane tab="Feed Items Status" key="feedItemStatus">
+          <TabPane tab="Feed Item Status" key="feedItemStatus">
             <ItemList name="feedItemStatus" />
           </TabPane>
-          <TabPane tab="Status (Promotions)" key="promoStatus">
+          <TabPane tab="Promotion Status" key="promoStatus">
             <ItemList name="promoStatus" />
           </TabPane>
           <TabPane tab="Checkout Type" key="checkoutType">
