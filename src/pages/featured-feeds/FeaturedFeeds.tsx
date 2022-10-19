0cbcda496eb4a1a696f280a3e197290b8f81cb51
Beatriz Rochelle <beatriz@hoxwi.com>
2022-10-19 14:12:04 +0200

    fix: returned filter to featured feeds

--- src/pages/featured-feeds/FeaturedFeeds.tsx
+++ src/pages/featured-feeds/FeaturedFeeds.tsx
@@ -1,19 +1,20 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Button, Col, PageHeader, Row, Table } from 'antd';
+import { Button, Col, PageHeader, Row, Select, Table, Typography } from 'antd';
 import { ColumnsType } from 'antd/lib/table';
-import { FeedItem } from 'interfaces/FeedItem';
+import { FeedItem } from '../../interfaces/FeedItem';
 import React, { useCallback, useContext, useRef, useState } from 'react';
-import { AppContext } from 'contexts/AppContext';
+import { AppContext } from '../../contexts/AppContext';
 import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
 import {
   fetchFeaturedFeeds,
   saveFeaturedFeeds,
-} from 'services/DiscoClubService';
+} from '../../services/DiscoClubService';
 import '@pathofdev/react-tag-input/build/index.css';
-import { useRequest } from 'hooks/useRequest';
+import { useRequest } from '../../hooks/useRequest';
 import { DndProvider, useDrag, useDrop } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import update from 'immutability-helper';
+import { useSelector } from 'react-redux';
 import { SearchOutlined } from '@ant-design/icons';
 
 interface DraggableBodyRowProps
@@ -25,6 +26,9 @@ interface DraggableBodyRowProps
 const type = 'DraggableBodyRow';
 
 const FeaturedFeed: React.FC<RouteComponentProps> = () => {
+  const {
+    settings: { feedList = [] },
+  } = useSelector((state: any) => state.settings);
   const [loading, setLoading] = useState(false);
   const [lastViewedIndex, setLastViewedIndex] = useState<number>(-1);
   const [list, setList] = useState<any[]>([]);
@@ -148,13 +152,46 @@ const FeaturedFeed: React.FC<RouteComponentProps> = () => {
     [listBuffer]
   );
 
+  const filterOption = (input: string, option: any) => {
+    return option?.label?.toUpperCase().includes(input?.toUpperCase());
+  };
+
   return (
     <>
       <div className="video-feed mb-1">
         <PageHeader
           title="Featured Feeds"
           subTitle={isMobile ? '' : 'List of Featured Feeds'}
-          extra={
+        />
+        <Row
+          justify="space-between"
+          align="bottom"
+          className="mb-05 sticky-filter-box"
+        >
+          <Col lg={4} xs={24}>
+            <Typography.Title level={5}>List Name</Typography.Title>
+
+            <Select
+              style={{ width: '100%' }}
+              onChange={value => fetch(value)}
+              placeholder="List name"
+              showSearch
+              allowClear
+              disabled={!feedList.length || loading}
+              filterOption={filterOption}
+            >
+              {feedList.map((curr: any) => (
+                <Select.Option
+                  key={curr.value}
+                  value={curr.value}
+                  label={curr.name}
+                >
+                  {curr.name}
+                </Select.Option>
+              ))}
+            </Select>
+          </Col>
+          <Col lg={8} xs={24}>
             <Row
               justify="end"
               align="middle"
@@ -184,8 +221,8 @@ const FeaturedFeed: React.FC<RouteComponentProps> = () => {
                 </Button>
               </Col>
             </Row>
-          }
-        />
+          </Col>
+        </Row>
         <DndProvider backend={HTML5Backend}>
           <Table
             scroll={{ x: true }}
