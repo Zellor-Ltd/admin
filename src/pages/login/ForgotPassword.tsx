c28d375d150b72aa874cc73fba14725dedce03bb
Beatriz <beatriz@hoxwi.com>
2023-07-10 00:46:58 -0700

    wip: forgot password flow

---
+++ src/pages/login/ForgotPassword.tsx
@@ -0,0 +1,90 @@
+import { Button, Col, Form, Image, Input, Row, Typography } from 'antd';
+import { useState } from 'react';
+import { Link, RouteComponentProps } from 'react-router-dom';
+
+const ForgotPassword: React.FC<RouteComponentProps> = props => {
+  const { history } = props;
+  const [loading, setLoading] = useState(false);
+
+  const onFinish = async (values: any) => {
+    setLoading(true);
+    try {
+      const response: any = await console.log('hi'); /* loginService(values) */
+      if (response.success) {
+        localStorage.setItem('token', response.token);
+        history.push('/');
+      }
+    } catch (e) {}
+    setLoading(false);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <>
+      <div className="external-wrapper">
+        <div className="external-container">
+          <Image width="100%" style={{ padding: '5% 10%' }} src="/logo.svg" />
+          <Typography.Title level={4}>
+            <strong>New Password</strong>
+          </Typography.Title>
+          <Form
+            name="auth"
+            layout="vertical"
+            className="mt-075"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            requiredMark={false}
+            onFinishFailed={onFinishFailed}
+          >
+            <Form.Item
+              label={<strong>Password</strong>}
+              name="pwd"
+              className="mb-05"
+              rules={[
+                { required: true, message: 'Please input your password!' },
+              ]}
+            >
+              <Input.Password allowClear />
+            </Form.Item>
+            <Row justify="end" style={{ alignItems: 'baseline' }}>
+              <Col className="mb-05">
+                <Typography.Text type="secondary">
+                  <strong>Forgot your password?</strong>
+                </Typography.Text>
+              </Col>
+              <Col span={24}>
+                <Form.Item className="mb-1">
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    style={{ background: 'rgb(48,86,211)', width: '100%' }}
+                    loading={loading}
+                  >
+                    Sign In
+                  </Button>
+                </Form.Item>
+              </Col>
+              <Col span={24} className="mb-1">
+                <Row justify="center">
+                  <Col>
+                    <Typography.Text type="secondary" className="mr-15">
+                      Don't have an account yet?
+                    </Typography.Text>
+                  </Col>
+                  <Col>
+                    <Link to="/sign-up">Sign Up</Link>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ForgotPassword;
